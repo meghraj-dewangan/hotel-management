1cb416c70dfe5abc8bad039b46a02a62f1b6a162
meghraj-dewangan <163406750+meghraj-dewangan@users.noreply.github.com>
2025-05-26 20:42:25 +0530

    add staff and booking count

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Staff;
+use App\Models\Booking;
 
 class AdminController extends Controller
 {
     //
 
     function admingpage(){
-        return view("admin");
+        $staffs = Staff::get(); 
+        $bookings = Booking::get();
+
+        return view("admin",['staffs'=>$staffs,'bookings'=>$bookings]);
     }
 }
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,25 +5,33 @@
 
 
     <!-- Main Content -->
-    <div class="main-content">
-        <div class="header d-flex flex-column flex-sm-row bg-white">
-            <h1>Dashboard</h1>
-            <input type="text" class="search-bar" placeholder="Search...">
-        </div>
+    <div class="main-content ">
+         <div class="header text-center mb-5">
+            
+        <h1>Dashboard</h1>
+         
+    </div>
        
         <!-- Cards Section -->
         <div class="cards">
+            
             <div class="card cursor-pointer text-center">
                 <h3>Customers</h3>
-                <p>Total Registered: 1,245</p>
-                <a href="#" class=" btn btn-card">View Details</a>
+                
+                           
+                <p>Total Registered: {{ count($bookings) }}</p>
+               
             </div>
+              
 
+            
             <div class="card cursor-pointer text-center">
                 <h3>Staff</h3>
-                <p>Total Staff: 224</p>
-                <a href="#" class=" btn btn-card">View Details</a>
+
+                <p>Total Staff: {{ count($staffs) }}</p>
+               
             </div>
+             
         </div>
     </div>
 
